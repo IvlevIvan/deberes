234d082d872af712c4d3c89769a6b4abd2e63e4b
Ivan Ivlev <ivlevivan@inbox.ru>
2016-09-29 18:15:16 +0200

    Add files via upload

---
+++ dz3.js
@@ -0,0 +1,8 @@
+ for (var i=1;i<=8;i++){
+           var a=1
+            while (a<=i){
+                document.write('#');
+                a=a+1
+            }
+            document.write("<br/>");
+       }
